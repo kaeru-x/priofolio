032e23f64f1693d3db15d5e36bce48f714cf6070
Haruka Sasaki <sasaki@hakoreco.com>
2021-07-09 22:37:46 +0900

    fix: nuxt.sonfig pakage

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,4 +44,18 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
+
+}
+
+const environment = process.env.NODE_ENV || 'local'
+const env = require(`./env/${environment}.ts`)
+
+// 気になればログ吐いてデバッグ
+console.log(env)
+
+module.exports = {
+  router: {
+    base: env.BASE_URL
+  }
+  env: env
 }
